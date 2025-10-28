3f1d7a1e4fd91c4437bb18202a97f7dfc5b48d68
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-27 20:58:35 -0400

    Add files via upload

---
+++ sound-space-plus-appimage.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+VERSION="$(cat ~/version)"
+
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export DESKTOP=DUMMY
+export ICON=https://raw.githubusercontent.com/David20122/sound-space-plus/refs/heads/nightly/assets/images/branding/icon.png
+export OUTNAME=SoundSpacePlus-"$VERSION"-anylinux-"$ARCH".AppImage
+export DEPLOY_OPENGL=1
+export DEPLOY_PIPEWIRE=1
+
+# DEPLOY ALL LIBS
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun ./AppDir/bin/*
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
+
