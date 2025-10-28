44bd05317277d70af33aa0bd14e386bf2f0bcb09
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-28 13:36:31 -0400

    use `DEPLOY_SDL`

--- sound-space-plus-appimage.sh
+++ sound-space-plus-appimage.sh
@@ -11,13 +11,14 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/David20122/sound-space-plus/refs/heads/nightly/assets/images/branding/icon.png
 export OUTNAME=SoundSpacePlus-"$VERSION"-anylinux-"$ARCH".AppImage
+export DEPLOY_SDL=1
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # DEPLOY ALL LIBS
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
-./quick-sharun ./AppDir/bin/* /usr/lib/libSDL*
+./quick-sharun ./AppDir/bin/*
 
 # MAKE APPIMAGE WITH URUNTIME
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
