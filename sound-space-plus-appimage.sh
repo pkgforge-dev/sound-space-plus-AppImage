aa2e372bcd59eb2b662bb5fa14d497ec90eeb6b7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-06 06:10:19 -0400

    simplify

--- sound-space-plus-appimage.sh
+++ sound-space-plus-appimage.sh
@@ -3,10 +3,10 @@
 set -eux
 
 ARCH="$(uname -m)"
-URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 VERSION="$(cat ~/version)"
 
+export OUTPUT_APPIMAGE=1
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/David20122/sound-space-plus/refs/heads/nightly/assets/images/branding/icon.png
@@ -19,11 +19,6 @@ export DEPLOY_PIPEWIRE=1
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun ./AppDir/bin/*
-
-# MAKE APPIMAGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
-chmod +x ./uruntime2appimage
-./uruntime2appimage
 
 mkdir -p ./dist
 mv -v ./*.AppImage*  ./dist
