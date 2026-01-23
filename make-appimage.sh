2c81e4a431b6fd7bd7db3af3bc60d67742067892
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-23 17:40:07 -0400

    update to template

---
+++ make-appimage.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+export ARCH
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=https://raw.githubusercontent.com/David20122/sound-space-plus/refs/heads/nightly/assets/images/branding/icon.png
+export DEPLOY_SDL=1
+export DEPLOY_OPENGL=1
+
+# Deploy dependencies
+quick-sharun ./AppDir/bin/*
+
+# Additional changes can be done in between here
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
