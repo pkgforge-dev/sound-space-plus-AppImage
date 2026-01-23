3e9b13b9ce4c37ad98d8a3fde1aa21e6a2d588bb
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-23 17:37:42 -0400

    update to template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,27 +1,17 @@
 #!/bin/sh
 
-set -eux
-ARCH="$(uname -m)"
-BINARY="https://github.com/David20122/sound-space-plus/releases/latest/download/linux.zip"
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+set -eu
 
-echo "Installing dependencies..."
+ARCH=$(uname -m)
+BINARY="https://github.com/David20122/sound-space-plus/releases/latest/download/linux.zip"
+
+echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	base-devel        \
-	curl              \
-	git               \
-	libx11            \
-	libxrandr         \
-	libxss            \
-	pulseaudio        \
-	pulseaudio-alsa   \
-	pipewire-audio    \
-	sdl2              \
-	unzip             \
-	wget              \
-	xorg-server-xvfb  \
-	zsync
+pacman -Syu --noconfirm sdl2 unzip
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+get-debloated-pkgs --add-common --prefer-nano
 
 echo "Getting binary..."
 echo "---------------------------------------------------------------"
@@ -34,11 +24,3 @@ awk -F'/' '/Location:/{print $(NF-1); exit}' /tmp/download.log > ~/version
 mkdir -p ./AppDir/bin
 unzip /tmp/download.zip -d ./AppDir/bin
 chmod +x ./AppDir/bin/SoundSpacePlus."$ARCH"
-
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common
-
-
