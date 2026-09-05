d5a1682f122c0b91524be9a368c87ba9e494db62
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-04 20:19:03 -0400

    Update template and hook (#15)
    * Update make-appimage.sh
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Link4Electronics <link4electronics@gmail.com>

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/David20122/sound-space-plus/refs/heads/nightly/assets/images/branding/icon.png
 export DEPLOY_SDL=1
